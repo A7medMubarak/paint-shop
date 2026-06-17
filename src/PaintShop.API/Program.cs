a2050d704dfbe842982f36e25281692afd8a2a15
A7medMubarak <a7med.ebrahimg@gmail.com>
2026-06-18 02:52:55 +0300

    Add test project to solution, enable browser on F5 with root→frontend redirect

--- src/PaintShop.API/Program.cs
+++ src/PaintShop.API/Program.cs
@@ -21,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.MapGet("/", () => Results.Redirect("http://localhost:5173"));
 }
 
 app.UseMiddleware<GlobalExceptionHandler>();
